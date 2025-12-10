7d5fcf75117930737a595f1be7bf7fab8a63d8b0
Kacper Ptaszek <kacper.pt119@gmail.com>
2025-12-10 16:05:34 +0100

    Added fields: role, created_at to response json in user endpoints

--- backend/src/main/java/com/example/drones/user/dto/UserResponse.java
+++ backend/src/main/java/com/example/drones/user/dto/UserResponse.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class UserResponse {
     private String username;
+    private String role;
     private String name;
     private String surname;
     private String email;
 
     @JsonProperty("phone_number")
     private String phoneNumber;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
 }
