589d23f91264c0ca4ced41ed626310ab2e1a5847
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2026-01-20 18:44:08 +0100

    Enhanced email notifications for order acceptance and client acceptance and improve email content formatting

--- backend/src/main/java/com/example/drones/user/UserRepository.java
+++ backend/src/main/java/com/example/drones/user/UserRepository.java
@@ -22,21 +22,22 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByIdWithPortfolio(UUID userId);
 
     @Query(value = """
-            SELECT u.* FROM users u
-            JOIN operator_service os ON u.id = os.operator_id
+            SELECT u FROM UserEntity u
+            LEFT JOIN FETCH u.portfolio
+            JOIN OperatorServicesEntity os ON u.id = os.operatorId
             WHERE u.role = 'OPERATOR'
               AND u.id != :creatorId
-              AND os.service_name = :serviceName
+              AND os.serviceName = :serviceName
               AND u.coordinates IS NOT NULL
               AND u.radius IS NOT NULL
               AND (
                   6371 * acos(
-                      cos(radians(:orderLat)) * cos(radians(CAST(SPLIT_PART(u.coordinates, ',', 1) AS DOUBLE PRECISION))) *
-                      cos(radians(CAST(SPLIT_PART(u.coordinates, ',', 2) AS DOUBLE PRECISION)) - radians(:orderLon)) +
-                      sin(radians(:orderLat)) * sin(radians(CAST(SPLIT_PART(u.coordinates, ',', 1) AS DOUBLE PRECISION)))
+                      cos(radians(:orderLat)) * cos(radians(CAST(FUNCTION('SPLIT_PART', u.coordinates, ',', 1) AS double))) *
+                      cos(radians(CAST(FUNCTION('SPLIT_PART', u.coordinates, ',', 2) AS double)) - radians(:orderLon)) +
+                      sin(radians(:orderLat)) * sin(radians(CAST(FUNCTION('SPLIT_PART', u.coordinates, ',', 1) AS double)))
                   )
               ) <= u.radius
-            """, nativeQuery = true)
+            """)
     List<UserEntity> findMatchingOperators(
             @Param("serviceName") String serviceName,
             @Param("orderLat") double orderLat,
